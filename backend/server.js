b657be12c8c479b6f3636462ca5b9a4aa0a8011d
tanmay-exits <tanmayrewale232021@gmail.com>
2025-02-22 23:50:27 +0530

    added user controller and routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
 import dotenv from 'dotenv'
 import connectMongoDB from './db/connectMongoDB.js'
 import cookieParser from 'cookie-parser'
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended: true}))   // to parse form data(urlencoded
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
