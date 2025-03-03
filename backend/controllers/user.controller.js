25f789079f00246eb3d7bd22372cd484a40dd9bc
tanmay-exits <tanmayrewale232021@gmail.com>
2025-03-03 23:42:45 +0530

    .

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -70,12 +70,49 @@ export const getSuggestedUsers = async(req, res) => {
       }
     ])
     // filterout the users that i'm already following
-    const filteredUsers = users.filter(user => !userFollowedByMe.following.includes(users._id))
+    const filteredUsers = users.filter(user => !userFollowedByMe.following.includes(user._id))
     const suggestedUsers = filteredUsers.slice(0,4)
     suggestedUsers.forEach(user => user.password=null)
     res.status(200).json(suggestedUsers)
   } catch (error) {
     console.log('Error in getSuggestedUsers: ', error.message)
+    res.status(500).json({error: error.message})
+  }
+}
+
+export const updateUserProfile = async(req, res) => {
+  const {fullName, email, username, currentPassword, newPassword, bio, link} = req.body
+  let {profileImg, coverImg} = req.body
+  
+  const userId = req.user._id
+  try {
+    const user = await User.findById(userId)
+    if(!user) return res.status(400).json({message: 'User not found'})
+
+    if((!newPassword && currentPassword) || (!currentPassword && newPassword)){
+      return res.status(400).json({error: 'Please provide both current password and new password'})
+    }
+
+    if(currentPassword && newPassword){
+      const isMatch = await bcrypt.compare(currentPassword, user.password)
+      if(!isMatch) return res.status(400).json({error: 'Current password is incorrect'})
+      if(newPassword.length < 6){
+        return res.json(400).json({error: 'Password must be atleast 6 characters long'})
+      }
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(newPassword, salt)
+    }
+    
+    if(profileImg){
+
+    }
+
+    if(coverImg){
+
+    }
+
+  } catch (error) {
+    console.log('Error in updateUserProfile controller:', error.message)
     res.status(500).json({error: error.message})
   }
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import {protectRoute} from '../middleware/protectRoute.js'
-import {getUserProfile, followUnfollowUser, getSuggestedUsers} from '../controllers/user.controller.js'
+import {getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile} from '../controllers/user.controller.js'
 
 const router = express.Router()
 
 router.get('/profile/:username', protectRoute, getUserProfile)
 router.get('/suggested', protectRoute, getSuggestedUsers)
 router.post('/follow/:id', protectRoute, followUnfollowUser)
-// router.get('/update', protectRoute, updateUserProfile)
+router.get('/update', protectRoute, updateUserProfile)
 
 export default router
