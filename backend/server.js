289b2e37818a7268ef1c1db7b6f782ec135d8170
tanmay-exits <tanmayrewale232021@gmail.com>
2025-05-04 16:08:31 +0530

    missed a closing parenthesis

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'frontend/dist'))
+  app.use(express.static(path.join(__dirname, 'frontend/dist')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   })
