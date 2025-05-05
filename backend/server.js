edfc96749076a8d1ad0c0e52964f7ef9d13a2b94
tanmay-exits <tanmayrewale232021@gmail.com>
2025-05-05 13:58:54 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -25,6 +26,12 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Enable CORS
+app.use(cors({
+  origin: 'https://xclone-frontend.vercel.app', // Replace with your frontend URL
+  credentials: true, // Allow cookies to be sent with requests
+}));
+
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -33,13 +40,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
+app.get('/', (req, res) => {
+  return res.status(200).json('hello world')
+})
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(join(__dirname, '..', 'frontend', 'dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(join(__dirname, '..', 'frontend', 'dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+//   });
+// }
 
 connectMongoDB()
   .then(() => {
